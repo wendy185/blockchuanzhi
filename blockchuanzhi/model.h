d101523786e587f3929f6de96970a77ce56554e4
hebai <977841428@qq.com>
2016-01-25 18:49:28 +0800

    添加文件

---
+++ blockchuanzhi/model.h
@@ -0,0 +1,13 @@
+//
+//  model.h
+//  blockchuanzhi
+//
+//  Created by 何白 on 16/1/25.
+//  Copyright © 2016年 媒立方传媒科技. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface model : NSObject
+
+@end
---
+++ blockchuanzhi/model.m
@@ -0,0 +1,13 @@
+//
+//  model.m
+//  blockchuanzhi
+//
+//  Created by 何白 on 16/1/25.
+//  Copyright © 2016年 媒立方传媒科技. All rights reserved.
+//
+
+#import "model.h"
+
+@implementation model
+
+@end
